69c9dc45dff9b959632ad7ab5fee4edce0178357
Mark <giraftw2002@gmail.com>
2018-05-11 18:14:12 +0800

    back button to fab

--- src/app/customers/dashboard/dashboard.component.ts
+++ src/app/customers/dashboard/dashboard.component.ts
@@ -10,7 +10,7 @@ declare let gtag: Function;
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
-  styleUrls: ['./dashboard.component.css']
+  styleUrls: ['./dashboard.component.css'],
 })
 export class DashboardComponent implements OnInit {
   prods: Observable<Prod[]>;
